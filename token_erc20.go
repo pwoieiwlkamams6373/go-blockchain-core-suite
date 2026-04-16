9a4a44a431f3a75ab80db608d18b7e6787f89de8
pwoieiwlkamams6373 <pwoieiwlkamams6373@cdzhongguan.com>
2026-04-16 23:07:51 +0800

    Create token_erc20.go

---
+++ token_erc20.go
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract GoChainToken {
+    string public name = "GoChain Token";
+    string public symbol = "GCT";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowances;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(uint256 _totalSupply) {
+        totalSupply = _totalSupply * (10 ** uint256(decimals));
+        balances[msg.sender] = totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return balances[account];
+    }
+
+    function transfer(address to, uint256 amount) public returns (bool) {
+        require(balances[msg.sender] >= amount);
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool) {
+        allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+}
